ed1d6edbfffeabd508d381995e25b980a550318b
Christopher Ayala <50795649+ninjachrisxd@users.noreply.github.com>
2019-05-29 17:09:41 +0200

    Update number_recognition_cnn.py

--- number_recognition_cnn.py
+++ number_recognition_cnn.py
@@ -1,3 +1,14 @@
+# Convolutional Neural Network
+
+# Installing Theano
+# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
+
+# Installing Tensorflow
+# pip install tensorflow
+
+# Installing Keras
+# pip install --upgrade keras
+
 # Part 1 - Building the CNN
 
 # Importing the Keras libraries and packages
