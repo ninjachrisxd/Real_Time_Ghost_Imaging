f58e96e434f1f064dfd03e1f966a97003aa5a347
Christopher Ayala <50795649+ninjachrisxd@users.noreply.github.com>
2019-05-29 12:55:14 +0200

    Update number_recognition_num_predict.py

--- number_recognition_num_predict.py
+++ number_recognition_num_predict.py
@@ -1,51 +1,16 @@
-#Takes the Neural Net created by program "number_recognition.py" to predict a given photo
+import numpy as np
+from keras.models import load_model
+from keras.preprocessing import image
 
-import cv2
-import tensorflow as tf
-import matplotlib.pyplot as plt
+classifier = load_model('4cnn.h5')
 
-CATEGORIES = ["3", "4"]
-
-def prepare (filepath):
-    IMG_SIZE = 100
-    img_array = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
-    new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
-    return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
-
-model = tf.keras.models.load_model("4_or_not.model")
-
-prediction = model.predict([prepare('corrupted/SmallNumber3.png')])
-print(CATEGORIES[int(prediction[0][0])])
-
-prediction = model.predict([prepare('corrupted/SmallNumber4.png')])
-print(CATEGORIES[int(prediction[0][0])])
-
-prediction = model.predict([prepare('corrupted/3-64-0.tif')])
-print(CATEGORIES[int(prediction[0][0])])
-
-prediction = model.predict([prepare('corrupted/4-32-5.tif')])
-print(CATEGORIES[int(prediction[0][0])])
-
-prediction = model.predict([prepare('corrupted/3-64-10.tif')])
-print(CATEGORIES[int(prediction[0][0])])
-
-prediction = model.predict([prepare('corrupted/4-32-15.tif')])
-print(CATEGORIES[int(prediction[0][0])])
-
-prediction = model.predict([prepare('corrupted/3-64-20.tif')])
-print(CATEGORIES[int(prediction[0][0])])
-
-prediction = model.predict([prepare('corrupted/4-32-25.tif')])
-print(CATEGORIES[int(prediction[0][0])])
-
-prediction = model.predict([prepare('corrupted/3-64-30.tif')])
-print(CATEGORIES[int(prediction[0][0])])
-
-prediction = model.predict([prepare('corrupted/4-32-35.tif')])
-print(CATEGORIES[int(prediction[0][0])])
-
-prediction = model.predict([prepare('corrupted/3-64-40.tif')])
-print(CATEGORIES[int(prediction[0][0])])
-
-prediction = model.predict([prepare('corrupted/4-32-45.tif')])
-print(CATEGORIES[int(prediction[0][0])])
+test_image = image.load_img('corrupted/82.tiff', target_size = (64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = classifier.predict(test_image)
+if result[0][0] == 0:
+    prediction = '3'
+else:
+    prediction = '4'
+    
+print(int(prediction))
