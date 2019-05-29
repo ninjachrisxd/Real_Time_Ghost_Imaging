7c67ce108791b55cef904c369e6165f0c4af4d53
Christopher Ayala <50795649+ninjachrisxd@users.noreply.github.com>
2019-05-29 17:09:14 +0200

    Update number_recognition_num_predict.py

--- number_recognition_num_predict.py
+++ number_recognition_num_predict.py
@@ -1,16 +1,38 @@
+#Takes the Neural Net created by program "number_recognition.py" to predict a given photo
+
+import cv2
 import numpy as np
 from keras.models import load_model
 from keras.preprocessing import image
 
 classifier = load_model('4cnn.h5')
 
-test_image = image.load_img('corrupted/82.tiff', target_size = (64, 64))
+test_image = image.load_img('corrupted/test4.png', target_size = (64, 64))
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
-result = classifier.predict(test_image)
-if result[0][0] == 0:
-    prediction = '3'
-else:
+result = classifier.predict(test_image)[0] 
+
+if result == 0:
     prediction = '4'
+else:
+    prediction = 'Not 4'
     
-print(int(prediction))
+result = tuple(result)[0]
+print ("prediction: {}".format(prediction) )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
